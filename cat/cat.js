032794afccc04a162d5469e6772ece0fae499f3b
Jasmine Mao <jasmine8711@gmail.com>
2019-05-28 22:33:51 +0200

    cat rain

---
+++ cat/cat.js
@@ -0,0 +1,63 @@
+//one cat move
+function catMove(cat, startingX) {
+  let y = 0;
+  let x = startingX;
+  //to make the speed different
+  const acceleration = Math.floor(Math.random() * 7) + 3;
+
+  let id = setInterval(frame, 10);
+
+  function frame() {
+    if (y >= window.innerHeight) {
+      clearInterval(id);
+      cat.style.display = "none";
+    } else {
+      y = y + Math.random() * acceleration;
+      //x++;
+      cat.style.top = y + "px";
+      cat.style.left = x + "px";
+    }
+  }
+}
+
+let counter = 0;
+
+btn.onclick = () => {
+  //create <img>
+  let cat = new Image(100, 50); // width, height values are optional params
+  cat.src = "./image/cat.png";
+  //get the container
+  const catainer = document.getElementById("catainer");
+  //add cat to container
+  catainer.append(cat);
+  cat.style.position = "absolute";
+
+  catMove(cat, 0);
+
+  let id = setInterval(function() {
+    if (counter >= 20) {
+      counter = 0;
+      clearInterval(id);
+    }
+    counter++;
+
+    //create a lot of cats
+    for (let i = 0; i < 15; i++) {
+      let tmpCat = new Image(100, 50);
+      tmpCat.src = "./image/cat.png";
+      catainer.append(tmpCat);
+      x = Math.floor(Math.random() * window.innerWidth);
+      tmpCat.style.position = "absolute";
+      catMove(tmpCat, x);
+    }
+  }, 500);
+
+  const btn = document.getElementById("btn");
+  const body = document.querySelector("body");
+  const sun = document.getElementById("sun");
+  const audio = document.getElementById("bgMusic");
+  btn.innerHTML = "Stop the rain"; //button changes text
+  body.classList.toggle("bgbody"); // bg showed
+  sun.classList.toggle("none"); //sun gone
+  audio.play();
+};
--- cat/nyancat.js
+++ cat/nyancat.js
@@ -1,4 +1,4 @@
-const cat = document.getElementById("cat");
+const mao = document.getElementById("cat");
 const btn = document.querySelector(".btn");
 const body = document.querySelector("body");
 const sun = document.getElementById("sun");
@@ -6,8 +6,55 @@ const sun = document.getElementById("sun");
 const audio = document.getElementById("bgMusic");
 btn.onclick = () => {
   btn.innerHTML = "Stop the rain"; //button changes text
-  cat.classList.toggle("cat-active");
-  body.classList.toggle("bgbody");
-  sun.classList.toggle("none");
+  mao.classList.toggle("cat-active"); //first cat move
+  body.classList.toggle("bgbody"); // bg showed
+  sun.classList.toggle("none"); //sun gone
   audio.play(); //music play
 };
+//设置画布大小
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d");
+//得到img链接
+const img = new Image();
+img.src = "./image/cat.png";
+//设置img的数量
+const numOfCats = 25;
+let cats = [];
+for (let i = 0; i < numOfCats; i++) {
+  let x = Math.floor(Math.random() * canvas.width);
+  let y = Math.floor(Math.random() * canvas.height);
+  cats[i] = new cat(x, y);
+}
+
+//一只猫的动画
+function cat(x, y) {
+  this.x = x;
+  this.y = y;
+  //下雨
+  this.fall = function() {
+    this.y = this.y + 1;
+  };
+  //show用来显示之前得到的img图像
+  this.show = function() {
+    context.drawImage(img, this.x, this.y, 100, 50);
+  };
+}
+
+//基本的框架
+function draw() {
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < numOfCats; i++) {
+    cats[i].show(); //这多只猫去执行show这个function。
+  }
+}
+
+function update() {
+  draw();
+  window.requestAnimationFrame(update);
+}
+
+update();
